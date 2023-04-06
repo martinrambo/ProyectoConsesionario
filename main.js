7ddf019d8b4b9ad56e491d8d974f96c483027343
Diego <106757002+martinrambo@users.noreply.github.com>
2023-04-06 16:12:15 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-const modal = document.querySelector('.modal');
-const closeModal = document.querySelector('.modal__close');
-
-closeModal.addEventListener('click', (e)=>{
-    e.preventDefault();
-    modal.classList.remove('modal--show');
-
-});
